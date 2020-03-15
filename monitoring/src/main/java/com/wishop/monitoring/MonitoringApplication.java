e6c89ba532c9d26bb92d6a1580c46c774b347282
Nguyễn Duy Công <congcoi123@gmail.com>
2020-03-15 18:32:37 +0900

    feat: update template classes for monitoring service

---
+++ monitoring/src/main/java/com/wishop/monitoring/MonitoringApplication.java
@@ -0,0 +1,15 @@
+package com.wishop.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class MonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringApplication.class, args);
+    }
+
+}
