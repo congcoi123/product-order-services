2bd459dfb7891f04d0f7510debe6b1be2b1f7773
Nguyễn Duy Công <congcoi123@gmail.com>
2020-03-22 10:52:39 +0900

    refactor: the notification service

--- systems/api-gateway/src/main/java/com/wishop/gateway/GatewayApplication.java
+++ systems/api-gateway/src/main/java/com/wishop/gateway/GatewayApplication.java
@@ -29,7 +29,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -37,7 +36,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@RefreshScope
 @EnableDiscoveryClient
 @SpringBootApplication
 @Configuration
